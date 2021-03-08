1607ecbba685f015d74a29ff2617e3aefa294da4
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-03-08 15:45:12 +0400

    setup headerOption component and use it in Header.js to render home/community/chat/post icons

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import GroupIcon from '@material-ui/icons/Group';
+import EmailIcon from '@material-ui/icons/Email';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import Home from '@material-ui/icons/Home';
 const Header = () => {
 	return (
 		<div className="header">
@@ -11,7 +16,12 @@ const Header = () => {
 					<input type="text" />
 				</div>
 			</div>
-			<div className="header__right" />
+			<div className="header__right">
+				<HeaderOption title="chat" Icon={EmailIcon} />
+				<HeaderOption title="community" Icon={GroupIcon} />
+				<HeaderOption title="post" Icon={AddBoxIcon} />
+				<HeaderOption title="home" Icon={Home} myStyle="home" />
+			</div>
 		</div>
 	);
 };
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import './headerOption.css';
-const HeaderOption = () => {
-	return <div className="headerOption" />;
+
+const HeaderOption = ({ Icon, title, myStyle }) => {
+	return (
+		<div className="headerOption">
+			{myStyle ? (
+				Icon && (
+					<div className={myStyle}>
+						<Icon />
+					</div>
+				)
+			) : (
+				Icon && <Icon />
+			)}
+			<h6>{title}</h6>
+		</div>
+	);
 };
 
 export default HeaderOption;
