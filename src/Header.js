c423a234eec9c3d02a3f42c006a54506e7020d2d
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-03-09 19:20:19 +0400

    profile avatar setup

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,8 @@ import SearchIcon from '@material-ui/icons/Search';
 import HeaderOption from './HeaderOption';
 import GroupIcon from '@material-ui/icons/Group';
 import EmailIcon from '@material-ui/icons/Email';
-import AddBoxIcon from '@material-ui/icons/AddBox';
+// import AddBoxIcon from '@material-ui/icons/AddBox';
+import WorkIcon from '@material-ui/icons/Work';
 import Home from '@material-ui/icons/Home';
 const Header = () => {
 	return (
@@ -21,8 +22,9 @@ const Header = () => {
 			<div className="header__right">
 				<HeaderOption title="chat" Icon={EmailIcon} />
 				<HeaderOption title="community" Icon={GroupIcon} />
-				<HeaderOption title="post" Icon={AddBoxIcon} />
+				<HeaderOption title="jobs" Icon={WorkIcon} />
 				<HeaderOption title="home" Icon={Home} myStyle="home" />
+				<HeaderOption avatar="/images/avatar.png" myStyle="profile" />
 			</div>
 		</div>
 	);
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,15 +1,20 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './headerOption.css';
 
-const HeaderOption = ({ Icon, title, myStyle }) => {
+const HeaderOption = ({ Icon, title, myStyle,avatar }) => {
 	return (
 		<>
 			{Icon && (
 				<a href="/" className={(myStyle ? myStyle : '') + ' headerOption'}>
 					<Icon />
-					<h6>{title}</h6>
+					<h6>{title?title:''}</h6>
 				</a>
 			)}
+                   <a href="#">
+                     {avatar && <Avatar className={`${myStyle?myStyle:''}`} src={avatar}/>}
+                   </a> 
+            
 		</>
 	);
 };
