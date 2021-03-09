10864a9060fb0aed29a9506d845bc4ffc8e6c878
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-03-09 21:11:32 +0400

    display profile avatar in sidebar__top, and set a default background image on the top and style set global styles in app.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 function App() {
 	return (
-		<div>
+		<div className="app">
 			<Header />
+			<div className="container">
+				<Sidebar />
+			</div>
 		</div>
 	);
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,29 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './sidebar.css';
+
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			<div className="sidebar__top">
+				<img src="/images/bg.webp" alt="background image" />
+				<Avatar className="sidebar__avatar" />
+				<h2>sedki shgairi</h2>
+				<h4>Full stack web developper</h4>
+			</div>
+			<div className="sidebar__stats">
+				<div className="sidebar__stat">
+					<p>who viewed your profile</p>
+				</div>
+				<div className="sidebar__stat">
+					<p>your posts</p>
+				</div>
+			</div>
+			<div className="sidebar__bottom">
+				<p>recent</p>
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
