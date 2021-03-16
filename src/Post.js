395d48786bdd28951b107f918cdc54a5e35716df
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-03-16 17:07:47 +0400

    initial setup and style of post__buttons section

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,13 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import InputOptions from './InputOptions';
 import './post.css';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import TextsmsIcon from '@material-ui/icons/Textsms';
+import ShareIcon from '@material-ui/icons/Share';
+import SendIcon from '@material-ui/icons/Send';
+
 const Post = ({ name, description, message, photoUrl }) => {
 	return (
 		<div className="post">
@@ -13,6 +20,12 @@ const Post = ({ name, description, message, photoUrl }) => {
 			</div>
 			<div className="post__body">
 				<p>{message}</p>
+			</div>
+			<div className="post__buttons">
+				<InputOptions Icon={ThumbUpAltIcon} title="Like" color="#90a4ae" />
+				<InputOptions Icon={TextsmsIcon} title="Comment" color="#90a4ae" />
+				<InputOptions Icon={ShareIcon} title="Share" color="#90a4ae" />
+				<InputOptions Icon={SendIcon} title="Send" color="#90a4ae" />
 			</div>
 		</div>
 	);
