a03e1ff830ed8e9d2d980161feaa75b49e2b9e27
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-03-24 19:09:00 +0400

    initial authentication setup in redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { useSelector } from 'react-redux';
 import './App.css';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 function App() {
+	const user = useSelector(selectUser);
 	return (
 		<div className="app">
 			<Header />
-			<div className="container">
-				<Sidebar />
-				<Feed />
-			</div>
+			{!user ? (
+				<Login />
+			) : (
+				<div className="container">
+					<Sidebar />
+					<Feed />
+				</div>
+			)}
 		</div>
 	);
 }
---
+++ src/Login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './login.css';
+const Login = () => {
+	return <div className="login">3aslema from login</div>;
+};
+
+export default Login;
