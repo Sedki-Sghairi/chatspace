aae9b4d1fb9cddc9732a0ba01160a3b0323ae8c5
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-03-24 19:44:00 +0400

    login page initial setup

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import './login.css';
 const Login = () => {
-	return <div className="login">3aslema from login</div>;
+	const handleRegister = () => {
+		console.log('registering ...');
+	};
+	const handleLogin = () => {
+		console.log('login ...');
+	};
+
+	return (
+		<div className="login">
+			<div className="login__header">
+				<h1>
+					<span>Chat</span>Space
+				</h1>
+				<img src="/images/logo.svg" alt="logo" />
+			</div>
+			<form>
+				<input placeholder="Full name" type="text" />
+				<input placeholder="Email" type="email" />
+				<input placeholder="Password" type="password" />
+				<button onClick={handleLogin}>Log-In</button>
+			</form>
+			<p>
+				Not a member?{' '}
+				<button onClick={handleRegister} className="login__register">
+					Register Now
+				</button>
+			</p>
+		</div>
+	);
 };
 
 export default Login;
